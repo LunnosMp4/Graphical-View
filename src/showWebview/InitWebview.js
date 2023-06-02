6149333e7c70503eea25a55cd07d83daddfb5e1b
LunnosMp4 <loic.tisseyre@epitech.eu>
2023-06-02 19:53:45 +0200

    Clean code

--- src/showWebview/InitWebview.js
+++ src/showWebview/InitWebview.js
@@ -2,94 +2,79 @@ const vscode = require('vscode');
 let config = vscode.workspace.getConfiguration('graphical-view');
 let groups = config.get('groups');
 
-async function initWebview() {
+async function getFilenames() {
+    const files = await vscode.workspace.findFiles();
 
-    // Create and show a new webview
-    const panel = vscode.window.createWebviewPanel(
-        'node-view', // Webview identifier
-        'Node View', // Webview title
-        vscode.ViewColumn.One,
-        {
-            enableScripts: true,
-        }
+    const filteredFiles = files.filter((file) =>
+        file.path.match(/\.(js|html|css|c|h|cpp|rb|py|java|cs|swift|go|php|jsx|kt|rs|asm|s|hs|lhs|lisp|cl|pl|ps1|r|ts|vb|sh|m|h|ex|exs|erl|hrl|groovy|jl|ml|mli|sb2|sb3|st|tcl)$/i)
     );
+    const filenames = filteredFiles.map(file => file.fsPath.split('/').pop());
 
-    
-    async function getFilenames() {
-        const files = await vscode.workspace.findFiles();
+    return [filenames, filteredFiles];
+}
 
-        const filteredFiles = files.filter(file => file.path.endsWith('.js') || file.path.endsWith('.html') || file.path.endsWith('.css') || file.path.endsWith('.c') || file.path.endsWith('.h') || file.path.endsWith('.cpp') || file.path.endsWith('.rb') || file.path.endsWith('.py') || file.path.endsWith('.java') || file.path.endsWith('.cs') || file.path.endsWith('.swift') || file.path.endsWith('.go') || file.path.endsWith('.php') || file.path.endsWith('.jsx') || file.path.endsWith('.kt') || file.path.endsWith('.rs') || file.path.endsWith('.asm') || file.path.endsWith('.s') || file.path.endsWith('.hs') || file.path.endsWith('.lhs') || file.path.endsWith('.lisp') || file.path.endsWith('.cl') || file.path.endsWith('.pl') || file.path.endsWith('.ps1') || file.path.endsWith('.r') || file.path.endsWith('.ts') || file.path.endsWith('.vb') || file.path.endsWith('.sh') || file.path.endsWith('.m') || file.path.endsWith('.h') || file.path.endsWith('.ex') || file.path.endsWith('.exs') || file.path.endsWith('.erl') || file.path.endsWith('.hrl') || file.path.endsWith('.groovy') || file.path.endsWith('.jl') || file.path.endsWith('.ml') || file.path.endsWith('.mli') || file.path.endsWith('.sb2') || file.path.endsWith('.sb3') || file.path.endsWith('.st') || file.path.endsWith('.tcl'));
-    
-        const filenames = filteredFiles.map(file => file.fsPath.split('/').pop());
-    
-        return [filenames, filteredFiles];
-    }
-    
-    // Get filenames from the current workspace
-    let [files, filteredFiles] = await getFilenames();
+function getFileType(filename) {
+    let parts = filename.split(".");
+    return parts[parts.length - 1];
+}
 
-    function getFileType(filename) {
-        let parts = filename.split(".");
-        return parts[parts.length - 1];
-    }
+function getColorForFileType(fileType) {
+    const colorMap = {
+        html: '#e34c26',     // orange
+        css: '#563d7c',      // purple
+        js: '#f1e05a',       // yellow
+        c: '#555555',        // gray
+        h: '#555555',        // gray
+        cpp: '#f34b7d',      // pink
+        rb: '#701516',       // maroon
+        py: '#3572A5',       // blue
+        java: '#b07219',     // brown
+        cs: '#178600',       // green
+        swift: '#ffac45',    // orange
+        go: '#00ADD8',       // cyan
+        php: '#4F5D95',      // indigo
+        jsx: '#61dafb',     // light blue
+        kt: '#A97BFF',       // lavender
+        rs: '#DEA584',       // light brown
+        asm: '#808080',      // dark gray
+        s: '#808080',        // dark gray
+        hs: '#5E5086',       // dark purple
+        lhs: '#5E5086',      // dark purple
+        lisp: '#3FB68B',     // teal
+        cl: '#00ACD7',       // sky blue
+        pl: '#1E9FFF',       // dodger blue
+        ps1: '#012456',      // navy
+        r: '#198CE7',        // royal blue
+        ts: '#2b7489',       // dark cyan
+        vb: '#945DB7',       // violet
+        sh: '#89e051',       // lime green
+        m: '#f0db4f',        // dark yellow
+        ex: '#6E4A7E',       // dark lavender
+        exs: '#6E4A7E',      // dark lavender
+        erl: '#B83998',      // magenta
+        hrl: '#B83998',      // magenta
+        groovy: '#4298B8',   // medium blue
+        jl: '#8A7FDF',       // dark lavender
+        ml: '#DC8ADD',       // light lavender
+        mli: '#DC8ADD',      // light lavender
+        sb2: '#FFEF42',      // golden yellow
+        sb3: '#FFEF42',      // golden yellow
+        st: '#FF9B55',       // coral
+        tcl: '#E4CC98',      // tan
+    };
 
-    function getColorForFileType(fileType) {
-        const colorMap = {
-            html: '#e34c26',     // orange
-            css: '#563d7c',      // purple
-            js: '#f1e05a',       // yellow
-            c: '#555555',        // gray
-            h: '#555555',        // gray
-            cpp: '#f34b7d',      // pink
-            rb: '#701516',       // maroon
-            py: '#3572A5',       // blue
-            java: '#b07219',     // brown
-            cs: '#178600',       // green
-            swift: '#ffac45',    // orange
-            go: '#00ADD8',       // cyan
-            php: '#4F5D95',      // indigo
-            jsx: '#61dafb',     // light blue
-            kt: '#A97BFF',       // lavender
-            rs: '#DEA584',       // light brown
-            asm: '#808080',      // dark gray
-            s: '#808080',        // dark gray
-            hs: '#5E5086',       // dark purple
-            lhs: '#5E5086',      // dark purple
-            lisp: '#3FB68B',     // teal
-            cl: '#00ACD7',       // sky blue
-            pl: '#1E9FFF',       // dodger blue
-            ps1: '#012456',      // navy
-            r: '#198CE7',        // royal blue
-            ts: '#2b7489',       // dark cyan
-            vb: '#945DB7',       // violet
-            sh: '#89e051',       // lime green
-            m: '#f0db4f',        // dark yellow
-            ex: '#6E4A7E',       // dark lavender
-            exs: '#6E4A7E',      // dark lavender
-            erl: '#B83998',      // magenta
-            hrl: '#B83998',      // magenta
-            groovy: '#4298B8',   // medium blue
-            jl: '#8A7FDF',       // dark lavender
-            ml: '#DC8ADD',       // light lavender
-            mli: '#DC8ADD',      // light lavender
-            sb2: '#FFEF42',      // golden yellow
-            sb3: '#FFEF42',      // golden yellow
-            st: '#FF9B55',       // coral
-            tcl: '#E4CC98',      // tan
-        };
-    
-        return colorMap[fileType] || '#cccccc'; // default gray
-    }
+    return colorMap[fileType] || '#cccccc'; // default gray
+}
 
-    let groups = {};
-
-    function createNode(filename, filteredFiles) {
-        let fileType = getFileType(filename);
-        let filePath = filteredFiles.path;
+function createNodes(files, filteredFiles, groups) {
+    let nodes = [];
+    for (let i = 0; i < files.length; i++) {
+        let fileType = getFileType(files[i]);
+        let filePath = filteredFiles[i].path;
 
         let node = {
-            name: filename,
-            label: filename,
+            name: files[i],
+            label: files[i],
             group: fileType,
             color: getColorForFileType(fileType),
             path: filePath
@@ -103,16 +88,13 @@ async function initWebview() {
             groups[fileType].nodes.push(node);
         }
 
-        return node;
-    }
-
-    let nodes = [];
-    for (let i = 0; i < files.length; i++) {
-        let node = createNode(files[i], filteredFiles[i]);
         node.id = i;
         nodes.push(node);
     }
+    return nodes;
+}
 
+function createLinks(nodes, groups) {
     let links = [];
     for (let groupName in groups) {
         let group = groups[groupName];
@@ -126,6 +108,27 @@ async function initWebview() {
             links.push({source, target});
         }
     }
+    return links;
+}
+
+async function initWebview() {
+
+    // Create and show a new webview
+    const panel = vscode.window.createWebviewPanel(
+        'node-view', // Webview identifier
+        'Node View', // Webview title
+        vscode.ViewColumn.One,
+        {
+            enableScripts: true,
+        }
+    );
+    
+    // Get filenames from the current workspace
+    let [files, filteredFiles] = await getFilenames();
+    
+    let groups = {};
+    let nodes = createNodes(files, filteredFiles, groups)
+    let links = createLinks(nodes, groups)
 
     const html = `
     <!DOCTYPE html>
@@ -140,6 +143,7 @@ async function initWebview() {
             <script src="https://unpkg.com/react-force-graph-2d@1.23.13/dist/react-force-graph-2d.min.js"></script>
             <script src="https://d3js.org/d3.v5.min.js"></script>
         </body>
+        <button id="toggle-mode">Toggle Mode</button>
     </html>
 
     <style>
